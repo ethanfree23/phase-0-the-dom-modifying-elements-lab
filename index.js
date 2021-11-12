b7f2112e82291ad37460c0f539e2ff6e43ed183f
Ethan Freeman <ethanfree23@gmail.com>
2021-11-12 13:58:14 -0600

    Conmpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+//1
+const body = document.getElementsByTagName("body")[0];
+body.removeChild(body.querySelector("main"));
+
+//2
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Ethan is the champion"
